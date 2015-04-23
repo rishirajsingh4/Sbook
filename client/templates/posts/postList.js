9f700d802125a37d59c4b266f92aa389a101bbd8
rishiraj <rishirajsingh4@gmail.com>
2015-04-24 03:58:21 +0530

    comment implemented

--- client/templates/posts/postList.js
+++ client/templates/posts/postList.js
@@ -5,10 +5,16 @@ Template.postList.helpers({
 
     liked: function(){
         var post = Posts.findOne({_id: this._id, likers: Meteor.user()._id});
-        console.log(post);
+        //console.log(post);
         if(post!=null)
             return true;
     },
+
+    comments: function(){
+    	//console.log(this._id);
+    	console.log(Comments.find({postId:this._id}).fetch());
+    	return Comments.find({postId:this._id});
+    }
 });
 
 Template.postList.events({
@@ -16,6 +22,31 @@ Template.postList.events({
 		e.preventDefault();
         
         Meteor.call('likePost', this._id);
+	},
+
+	'submit form': function(e){
+		e.preventDefault();
+
+        var postId = this._id;
+        var cmnt = $(e.target).find('[name=comment]').val();
+
+        var comment = {};
+        comment.cmnt = cmnt;
+        comment.postId = postId;
+
+        /*console.log(comment.cmnt);
+        console.log(comment.ownerText);*/
+
+        Meteor.call('commentInsrt', comment, function(error, result) {
+            // display the error to the user and abort
+            if (error)
+                return alert(error.reason);
+
+            if(result){
+                $('#commentBox').val(''); 
+            }
+
+        });
 	}
 	
 });
